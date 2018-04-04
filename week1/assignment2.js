98bb13b2c4a119711139ebd1ee51c4899512dbaf
Enkhtulga <eta@Enkhtulgas-MacBook-Pro.local>
2018-04-03 21:12:19 -0500

    first commit

---
+++ week1/assignment2.js
@@ -0,0 +1,98 @@
+// setTimeout(function(){ 
+// 	console.log("world");
+// },2000);
+
+// console.log("hello");
+
+// setTimeout(() => {console.log('timeout');},0);
+// setImmediate(() => {console.log('immediate');});
+// process.nextTick(()=> console.log('nexttick'));
+
+
+Array.prototype.even  = function(){
+	var evens=[];
+	for(let value of this)
+	{
+		if(value%2==0)
+		{
+			evens.push(value);
+		}
+	}
+	return evens;
+}
+
+Array.prototype.odd  = function(){
+	var odds=[];
+	for(let value of this)
+	{
+		if(value%2==1)
+		{
+			odds.push(value);
+		}
+	}
+	return odds;
+}
+
+console.log([1,2,3,4,5,6,7,8].even());
+console.log([1,2,3,4,5,6,7,8].odd());
+
+// function asyncReal(data,callback){
+// 	process.nextTick(function(){ callback(data==='foo');});
+// }
+// console.log('done');
+
+function slow(callback){
+	// process.nextTick(function(){
+		for(let i=0;i<=5e8;i++){}
+		if(Math.random() >0.5){
+			return callback("Error",null)
+		}
+		callback(null,{id:12345})	
+	// });
+}
+
+function exec(fn){
+
+
+
+	// this.done = function(fn1)
+	// {
+	// 	fn1();
+	// 	return this;
+	// }
+	// this.fail = function(fn2)
+	// {
+	// 	fn2();
+	// 	return this;
+	// }
+	// setTimeout(fn,2000, );
+
+	setImmediate(function() {
+		fn(function(a,b){
+			if(a)  this._fail(a);
+			else this._done(b);
+		});
+		console.log("NONEBLOCK1");
+	});
+	
+	this.done = function(fn1)
+	{
+		this._done = fn1;
+		return this;
+	}
+	this.fail = function(fn2)
+	{
+		this._fail = fn2;
+		return this;
+	}
+
+
+	return this;
+	
+	
+};
+
+
+exec(slow).done(function(data){ console.log(data);})
+		  .fail(function(err){console.log("Error: "+err);});
+
